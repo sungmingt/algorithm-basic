d5b31fad94a4d7ed57c332d6ee662eb1db718e7e
sungmingt <qotjdals8313@naver.com>
2022-11-07 21:20:27 +0900

    commit : first commit

---
+++ src/main/java/basic/algorithm/Programmers/부족한_금액_구하기.java
@@ -0,0 +1,20 @@
+package basic.algorithm.Programmers;
+
+public class 부족한_금액_구하기 {
+
+    public static long solution(int price, int money, int count) {
+        long answer = 0;
+        int N = 0;
+
+        long result = recursive(answer, N, price, money, count) * -1;
+        return Math.max(result, 0);
+    }
+
+    private static long recursive(long answer, int N, int price, int money, int count) {
+        if(N == count) return money - answer;
+        N++;
+        answer += price * N;
+
+        return recursive(answer, N, price, money, count);
+    }
+}
