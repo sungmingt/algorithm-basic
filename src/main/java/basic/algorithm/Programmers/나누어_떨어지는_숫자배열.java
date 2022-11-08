17b1258a04f871bb71e207b0a476213b0c5b19d7
sungmingt <qotjdals8313@naver.com>
2022-11-08 21:58:11 +0900

    commit : 나누어_떨어지는_숫자배열

---
+++ src/main/java/basic/algorithm/Programmers/나누어_떨어지는_숫자배열.java
@@ -0,0 +1,11 @@
+package basic.algorithm.Programmers;
+
+import java.util.Arrays;
+
+public class 나누어_떨어지는_숫자배열 {
+
+    public int[] solution(int[] arr, int divisor) {
+        int[] filtered = Arrays.stream(arr).filter(num -> num % divisor == 0).sorted().toArray();
+        return filtered.length == 0 ? new int[]{-1} : filtered;
+    }
+}
