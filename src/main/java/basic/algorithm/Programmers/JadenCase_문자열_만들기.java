469a803c7ce292b06c22bfa814781631c3690f56
sungmingt <qotjdals8313@naver.com>
2022-11-09 17:25:50 +0900

    commit : JadenCase_문자열_만들기

---
+++ src/main/java/basic/algorithm/Programmers/JadenCase_문자열_만들기.java
@@ -0,0 +1,20 @@
+package basic.algorithm.Programmers;
+
+public class JadenCase_문자열_만들기 {
+
+    public static String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+
+        String words = s.toLowerCase();
+        boolean isFirst = true;
+
+        for (int i = 0; i < words.length(); i++) {
+            char c = words.charAt(i);
+            answer.append(isFirst ? String.valueOf(c).toUpperCase() : c);
+
+            isFirst = c == ' ';
+        }
+
+        return answer.toString();
+    }
+}
