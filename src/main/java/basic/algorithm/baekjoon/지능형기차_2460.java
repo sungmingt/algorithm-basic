a60b944cdd02574bf2db99e888e6e08bf2ba6d1d
sungmingt <qotjdals8313@naver.com>
2022-12-19 21:11:18 +0900

    commit : baekjoon/지능형기차_2460

---
+++ src/main/java/basic/algorithm/baekjoon/지능형기차_2460.java
@@ -0,0 +1,29 @@
+package basic.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class 지능형기차_2460 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int total = 10;
+        int max = 0;
+        int current = 0;
+
+        while (total > 0) {
+            int out = sc.nextInt();
+            int in = sc.nextInt();
+
+            current = current - out + in;
+            if (current > max) {
+                max = current;
+            }
+
+            total --;
+        }
+
+        System.out.println(max);
+    }
+}
