4efaa48497e4d7835ad6bb3018fa31fed8e57519
sungmingt <qotjdals8313@naver.com>
2022-12-19 21:26:27 +0900

    commit : baekjoon/피보나치수5_10870

---
+++ src/main/java/basic/algorithm/baekjoon/피보나치수5_10870.java
@@ -0,0 +1,22 @@
+package basic.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class 피보나치수5_10870 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
