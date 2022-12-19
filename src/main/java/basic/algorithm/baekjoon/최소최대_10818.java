288e0c51b0432ddd3d520b84e853d87b58350507
sungmingt <qotjdals8313@naver.com>
2022-12-19 21:06:52 +0900

    commit : baekjoon/최소최대_10818

---
+++ src/main/java/basic/algorithm/baekjoon/최소최대_10818.java
@@ -0,0 +1,25 @@
+package basic.algorithm.baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 최소최대_10818 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < count; i++) {
+            int number = sc.nextInt();
+
+            if(number>= max) max = number;
+            if(number<= min) min = number;
+        }
+
+        System.out.println(min + " " + max);
+    }
+}
