ddc05de46c735870176a92820ef93741f059c15d
sungmingt <qotjdals8313@naver.com>
2022-12-22 21:19:38 +0900

    commit : baekjoon/ProjectTeams_20044_greedy

---
+++ src/main/java/basic/algorithm/baekjoon/silver/ProjectTeams_20044_greedy.java
@@ -0,0 +1,28 @@
+package basic.algorithm.baekjoon.silver;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ProjectTeams_20044_greedy {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int memberCount = sc.nextInt() * 2;
+
+        int[] cap = new int[memberCount];
+        for (int i = 0; i < cap.length; i++) {
+            cap[i] = sc.nextInt();
+        }
+
+        Arrays.sort(cap);
+
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < cap.length; i++) {
+            int sum = cap[i] + cap[cap.length - 1 - i];
+            if(sum < min) min = sum;
+        }
+
+        System.out.println(min);
+    }
+}
