9da3ce58fed089b2ed40a0c7a41e2f3fe2ee9b53
sungmingt <qotjdals8313@naver.com>
2022-12-27 00:35:59 +0900

    commit : baekjoon/팩토리얼_10872_재귀

---
+++ src/main/java/basic/algorithm/baekjoon/팩토리얼_10872_재귀.java
@@ -0,0 +1,21 @@
+package basic.algorithm.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 팩토리얼_10872_재귀 {
+
+    static int sum = 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        System.out.println(factorial(N));
+    }
+
+    public static int factorial(int n) {
+        if(n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+}
