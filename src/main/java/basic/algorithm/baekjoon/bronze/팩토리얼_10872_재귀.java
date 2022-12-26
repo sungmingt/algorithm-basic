d0183ba849d267df5b615a0f90006a93700104a7
sungmingt <qotjdals8313@naver.com>
2022-12-27 00:58:24 +0900

    commit : baekjoon/재귀의귀재_25501_재귀

--- src/main/java/basic/algorithm/baekjoon/bronze/팩토리얼_10872_재귀.java
+++ src/main/java/basic/algorithm/baekjoon/bronze/팩토리얼_10872_재귀.java
@@ -1,4 +1,4 @@
-package basic.algorithm.baekjoon;
+package basic.algorithm.baekjoon.bronze;
 
 import java.io.BufferedReader;
 import java.io.IOException;
---
+++ src/main/java/basic/algorithm/baekjoon/재귀의귀재_25501_재귀.java
@@ -0,0 +1,38 @@
+package basic.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class 재귀의귀재_25501_재귀 {
+
+    static int recursionCount;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            String str = sc.next();
+
+            int palindrome = isPalindrome(str);
+            System.out.println(palindrome + " " + recursionCount);
+            recursionCount = 0;
+        }
+    }
+
+    public static int isPalindrome(String str) {
+        return recursion(str, 0, str.length() - 1);
+    }
+
+    private static int recursion(String str, int firstIndex, int secondIndex) {
+        recursionCount++;
+
+        if(firstIndex >= secondIndex) {
+            return 1;
+        }
+        if(str.charAt(firstIndex) != str.charAt(secondIndex)) {
+            return 0;
+        }
+
+        return recursion(str, firstIndex + 1, secondIndex - 1);
+    }
+}
