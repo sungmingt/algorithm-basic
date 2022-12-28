f8f75293b04ad32197d34ece8ad399c708eebf52
sungmingt <qotjdals8313@naver.com>
2022-12-28 21:21:43 +0900

    commit : baekjoon/좌표정렬하기_11650_2차원배열_정렬

---
+++ src/main/java/basic/algorithm/baekjoon/좌표정렬하기_11650_2차원배열_정렬.java
@@ -0,0 +1,30 @@
+package basic.algorithm.baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 좌표정렬하기_11650_2차원배열_정렬 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[0] == o2[0]) {
+                return o1[1] - o2[1];
+            } else {
+                return o1[0] - o2[0];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+    }
+}
