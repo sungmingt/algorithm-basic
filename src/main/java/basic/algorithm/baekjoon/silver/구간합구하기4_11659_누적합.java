430ba8534f281f1b60fdcf24ed3f9c40e5dedfd1
sungmingt <qotjdals8313@naver.com>
2022-12-29 16:28:46 +0900

    commit : baekjoon/구간합구하기4_11659_누적합

--- src/main/java/basic/algorithm/baekjoon/silver/구간합구하기4_11659_누적합.java
+++ src/main/java/basic/algorithm/baekjoon/silver/구간합구하기4_11659_누적합.java
@@ -14,23 +14,18 @@ public class 구간합구하기4_11659_누적합 {
             numberList[i] = sc.nextInt();
         }
 
-        // {1, 2, 3, 4, 5}
-        // {1, 3, 6, 10, 15}
-        // {1, (1) + 2, (1+2) + 3, (6+4), (10+5)
         for (int i = 1; i < numberList.length; i++) { //누적합 계산
             numberList[i] += numberList[i-1];
         }
 
         StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < M; i++) {
-            int start = sc.nextInt(); // 1
-            int end = sc.nextInt();       // 3
-            int sum = 0;
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            int result = 0;
 
-            sum = numberList[end] - numberList[start - 1];
-
-            sb.append(sum + "\n");
+            result = numberList[end] - numberList[start - 1];
+            sb.append(result + "\n");
         }
 
         System.out.println(sb);
