6b10097c1210d179b52e6302c764bf8f52107d49
sungmingt <qotjdals8313@naver.com>
2023-01-19 00:34:29 +0900

    commit : programmers/구명보트_Greedy

--- src/main/java/basic/algorithm/programmers/lv2/구명보트_Greedy.java
+++ src/main/java/basic/algorithm/programmers/lv2/구명보트_Greedy.java
@@ -7,7 +7,6 @@ public class 구명보트_Greedy {  //다시 풀어보기
     static int solution(int[] people, int limit) {
         Arrays.sort(people);
 
-        //반복문 돌며 무게들을 더하는데, 합이 limit 보다 커지면 종료
         int minIndex = 0;
         int result = 0;
 
