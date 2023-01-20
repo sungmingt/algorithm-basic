d243f2c6be729378ea5fc19cfb3c384eebc689e2
sungmingt <qotjdals8313@naver.com>
2023-01-21 00:54:13 +0900

    commit : programmers/완주하지못한선수_Hash

---
+++ src/main/java/basic/algorithm/programmers/lv2/완주하지못한선수_Hash.java
@@ -0,0 +1,24 @@
+package basic.algorithm.programmers.lv2;
+
+import java.util.HashMap;
+
+public class 완주하지못한선수_Hash {
+
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> hash = new HashMap<>();
+
+        for (String c : completion) {
+            hash.put(c, hash.getOrDefault(c, 0) + 1);
+        }
+
+        for (String s : participant) {
+            if (!hash.containsKey(s) || hash.get(s) == 0) {
+                return s;
+            }
+
+            hash.replace(s, hash.get(s) - 1);
+        }
+
+        return null;
+    }
+}
