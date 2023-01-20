06efad59da21a92b48ff235f529ab67f99d09a8c
sungmingt <qotjdals8313@naver.com>
2023-01-20 20:27:24 +0900

    commit : programmers/괄호회전하기_Stack

---
+++ src/main/java/basic/algorithm/programmers/lv2/괄호회전하기_Stack.java
@@ -0,0 +1,62 @@
+package basic.algorithm.programmers.lv2;
+
+import java.util.Stack;
+
+public class 괄호회전하기_Stack {
+
+    static int solution(String s) {
+        //0 ~ s.length() - 1 만큼 회전 가능
+        //회전한다 -> 첫 요소를 마지막 요소에 삽입
+
+        Stack<Character> globalStack = new Stack<>();
+        int answer = 0;
+        char[] chars = s.toCharArray();
+
+        //globalStack 초기화
+        for (char c : chars) {
+            globalStack.push(c);
+        }
+
+        for (int i = 0; i < chars.length; i++) {
+            Stack<Character> localStack = new Stack<>();
+            boolean isTrue = true;
+
+            //올바른 괄호인지 확인
+            for (char c : globalStack) {
+                if (c == '[' || c == '(' || c == '{') {
+                    localStack.push(c);
+                } else if (c == ']') {
+                    if (localStack.isEmpty() || localStack.peek() != '[') {
+                        isTrue = false;
+                        break;
+                    } else {
+                        localStack.pop();
+                    }
+                } else if (c == ')') {
+                    if (localStack.isEmpty() || localStack.peek() != '(') {
+                        isTrue = false;
+                        break;
+                    } else {
+                        localStack.pop();
+                    }
+                } else if (c == '}') {
+                    if (localStack.isEmpty() || localStack.peek() != '{') {
+                        isTrue = false;
+                        break;
+                    } else {
+                        localStack.pop();
+                    }
+                }
+            }
+
+            //스택에 괄호가 남아있거나 | 올바른 괄호가 아니라면
+            if (!(!localStack.isEmpty() || !isTrue)) {
+                answer++;
+            }
+
+            globalStack.push(globalStack.remove(0));
+        }
+
+        return answer;
+    }
+}
