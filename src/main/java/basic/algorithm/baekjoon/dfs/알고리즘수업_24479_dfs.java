bee4ef2dfe59d15be8beb9721177b344fd8f32fc
sungmingt <qotjdals8313@naver.com>
2023-02-12 15:19:30 +0900

    commit : baekjoon/알고리즘수업_24479_dfs

--- src/main/java/basic/algorithm/baekjoon/dfs/알고리즘수업_24479_dfs.java
+++ src/main/java/basic/algorithm/baekjoon/dfs/알고리즘수업_24479_dfs.java
@@ -1,67 +1,123 @@
 package basic.algorithm.baekjoon.dfs;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class 알고리즘수업_24479_dfs { //dfs 기본 (다시 풀어보기)
+public class 알고리즘수업_24479_dfs {
 
     static List<ArrayList<Integer>> graph;
-    static int[] checked;
-    static int visitOrder; //방문 순서
+    static boolean[] visited;
+    static Map<Integer, Integer> result;
+    static int visitOrder;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int vertex = sc.nextInt();  //정점
-        int edge = sc.nextInt();    //간선
-        int startVertex = sc.nextInt();  //시작 정점
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int R = sc.nextInt();
 
         graph = new ArrayList<>();
-        checked = new int[vertex + 1];
+        visited = new boolean[N + 1];
+        result = new HashMap<>(N + 1);
 
-        for (int i = 0; i < vertex + 1; i++) {
-            graph.add(new ArrayList());
+        for (int i = 0; i < N + 1; i++) {
+            graph.add(new ArrayList<>());
         }
 
-        // {[1, 2], [2, 3] ...}
+        //간선 입력
+        for (int i = 0; i < M; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
 
-        for (int i = 0; i < edge; i++) {
-            int fromVertex = sc.nextInt();
-            int toVertex = sc.nextInt();
-
-            graph.get(fromVertex).add(toVertex);
-            graph.get(toVertex).add(fromVertex);
+            graph.get(start).add(end);
+            graph.get(end).add(start);
         }
 
-        //오름차순으로 방문해야 하기 때문에 정렬
+        //오름차순 방문 -> 정렬
         for (int i = 1; i < graph.size(); i++) {
             Collections.sort(graph.get(i));
         }
 
-        visitOrder = 1;  //시작 정점도 방문 순서에 포함
+        visited[R] = true;
+        visitOrder = 1;
+        result.put(R, visitOrder);
+        dfs24479(R);
 
-        dfs(startVertex);
-
-        for (int i = 1; i < checked.length; i++) {
-            System.out.println(checked[i]);
+        for (int i = 1; i < N + 1; i++) {
+            System.out.println(result.getOrDefault(i, 0));
         }
-
     }
 
-    static void dfs(int vertex) {
-        checked[vertex] = visitOrder;
+    static void dfs24479(int startVertex) {
+        //오름차순 방문
+        for (int i = 0; i < graph.get(startVertex).size(); i++) {
+            Integer newVertex = graph.get(startVertex).get(i);
 
-        for (int i = 0; i < graph.get(vertex).size(); i++) {
-            int newVertex = graph.get(vertex).get(i);
-
-            if(checked[newVertex] == 0){ //방문 안 했을 경우
+            //방문하지 않았다면
+            if (!visited[newVertex]) {
+                visited[newVertex] = true;
                 visitOrder++;
-                dfs(newVertex);
+                result.put(newVertex, visitOrder);
+                dfs24479(newVertex);
             }
         }
-
-
     }
+
+
+
+
+//    static List<ArrayList<Integer>> graph;
+//    static int[] checked;
+//    static int visitOrder; //방문 순서
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int vertex = sc.nextInt();  //정점
+//        int edge = sc.nextInt();    //간선
+//        int startVertex = sc.nextInt();  //시작 정점
+//
+//        graph = new ArrayList<>();
+//        checked = new int[vertex + 1];
+//
+//        for (int i = 0; i < vertex + 1; i++) {
+//            graph.add(new ArrayList());
+//        }
+//
+//        // {[1, 2], [2, 3] ...}
+//
+//        for (int i = 0; i < edge; i++) {
+//            int fromVertex = sc.nextInt();
+//            int toVertex = sc.nextInt();
+//
+//            graph.get(fromVertex).add(toVertex);
+//            graph.get(toVertex).add(fromVertex);
+//        }
+//
+//        //오름차순으로 방문해야 하기 때문에 정렬
+//        for (int i = 1; i < graph.size(); i++) {
+//            Collections.sort(graph.get(i));
+//        }
+//
+//        visitOrder = 1;  //시작 정점도 방문 순서에 포함
+//
+//        dfs(startVertex);
+//
+//        for (int i = 1; i < checked.length; i++) {
+//            System.out.println(checked[i]);
+//        }
+//    }
+//
+//    static void dfs(int vertex) {
+//        checked[vertex] = visitOrder;
+//
+//        for (int i = 0; i < graph.get(vertex).size(); i++) {
+//            int newVertex = graph.get(vertex).get(i);
+//
+//            if(checked[newVertex] == 0){ //방문 안 했을 경우
+//                visitOrder++;
+//                dfs(newVertex);
+//            }
+//        }
+//    }
 }
