2fef7414694d1d44f48ec33ed8884df88b7460ca
sungmingt <qotjdals8313@naver.com>
2023-03-23 00:31:41 +0900

    commit : baekjoon/트럭_13335_Queue

--- src/main/java/basic/algorithm/baekjoon/backtracking/트럭_13335_Queue.java
+++ src/main/java/basic/algorithm/baekjoon/backtracking/트럭_13335_Queue.java
@@ -38,10 +38,8 @@ public class 트럭_13335_Queue {
                 if (totalWeight + curTruck <= L) {
                     q.offer(curTruck);
                     totalWeight += curTruck;
-
                     //다리 길이만큼 다 차면 맨 앞을 빼준다.
                     pollFirst(q);
-
                     break;
                 } else {
                     q.offer(0);
@@ -52,6 +50,7 @@ public class 트럭_13335_Queue {
             }
         }
 
+        //마지막 요소 진입 후 break 한다 -> 다 빠져나올때까지 W만큼 걸리기 때문에 W를 더해준다.
         System.out.println(time + W);
     }
 
