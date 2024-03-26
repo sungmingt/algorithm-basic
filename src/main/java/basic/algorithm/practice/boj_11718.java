821fc9a84dd4be65e80e5ee3654f8697dda46aed
sungmingt <qotjdals8313@naver.com>
2024-03-26 16:03:51 +0900

    commit : boj_11781

---
+++ src/main/java/basic/algorithm/practice/boj_11718.java
@@ -0,0 +1,17 @@
+package basic.algorithm.practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_11718 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+
+        while ((input = br.readLine()) != null) {
+            System.out.println(input);
+        }
+    }
+}
