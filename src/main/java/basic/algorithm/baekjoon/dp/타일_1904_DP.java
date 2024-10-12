e47670fb9e46abccaf20cfcea2ed65f17cd53c3b
sungmingt <qotjdals8313@naver.com>
2024-10-12 10:32:40 +0900

    boj_1904_DP

--- src/main/java/basic/algorithm/baekjoon/dp/타일_1904_DP.java
+++ src/main/java/basic/algorithm/baekjoon/dp/타일_1904_DP.java
@@ -4,24 +4,35 @@ import java.util.Scanner;
 
 public class 타일_1904_DP {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    static int[] dp;
+    static int N;
 
-        if(N == 1) {
+    public static void main(String[] args) {
+        //N:1 - 1 (1)
+        //N:2 - 2 (00, 11)
+        //N:3 - 3 (001, 111, 100)
+        //N:4 - 5 (0011, 0000, 1001, 1100, 1111)
+        //N:5 - 8 (00001, 11111, 00111, 10000, 00100, 11100, 10011, 11001)
+        //-> 피보나치
+
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        if (N == 1) {
             System.out.println(1);
             return;
         }
-        if(N == 2){
+
+        if (N == 2) {
             System.out.println(2);
             return;
         }
 
-        int[] dp = new int[N + 1];
+        dp = new int[N + 1];
         dp[1] = 1;
         dp[2] = 2;
 
-        for (int i = 3; i < N + 1; i++) {
+        for (int i = 3; i < dp.length; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
         }
 
