e8556d106b2a099d64bb0a2ac3e8326e6fcf886a
sungmingt <qotjdals8313@naver.com>
2024-10-13 12:55:34 +0900

    boj_25501_recursive

--- src/main/java/basic/algorithm/baekjoon/recursive/bronze/재귀의귀재_25501_재귀.java
+++ src/main/java/basic/algorithm/baekjoon/recursive/bronze/재귀의귀재_25501_재귀.java
@@ -1,38 +1,46 @@
 package basic.algorithm.baekjoon.recursive.bronze;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class 재귀의귀재_25501_재귀 {
 
-    static int recursionCount;
+    static int N;
+    static int callCount;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < N; i++) {
-            String str = sc.next();
+            callCount = 0;
+            String word = br.readLine();
 
-            int palindrome = isPalindrome(str);
-            System.out.println(palindrome + " " + recursionCount);
-            recursionCount = 0;
+            int bool = isPalindrome(word);
+            sb.append(bool + " " + callCount + "\n");
         }
+
+        System.out.println(sb);
     }
 
-    public static int isPalindrome(String str) {
-        return recursion(str, 0, str.length() - 1);
+    static int isPalindrome(String word) {
+        return recursion(word, 0, word.length() - 1);
     }
 
-    private static int recursion(String str, int firstIndex, int secondIndex) {
-        recursionCount++;
+    private static int recursion(String word, int st, int end) {
+        callCount++;
 
-        if(firstIndex >= secondIndex) {
+        if (st >= end) {
             return 1;
         }
-        if(str.charAt(firstIndex) != str.charAt(secondIndex)) {
+
+        if (word.charAt(st) != word.charAt(end)) {
             return 0;
         }
 
-        return recursion(str, firstIndex + 1, secondIndex - 1);
+        return recursion(word, st + 1, end - 1);
     }
 }
