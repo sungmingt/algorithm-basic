2061e927a9786dfd0ae7e2090592be63e6055354
sungmingt <qotjdals8313@naver.com>
2024-10-15 11:23:30 +0900

    boj_10872_recursive

--- src/main/java/basic/algorithm/baekjoon/recursive/bronze/팩토리얼_10872_재귀.java
+++ src/main/java/basic/algorithm/baekjoon/recursive/bronze/팩토리얼_10872_재귀.java
@@ -1,21 +1,18 @@
 package basic.algorithm.baekjoon.recursive.bronze;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class 팩토리얼_10872_재귀 {
 
-    static int sum = 1;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
 
         System.out.println(factorial(N));
     }
 
-    public static int factorial(int n) {
-        if(n <= 1) return 1;
-        return n * factorial(n - 1);
+    private static int factorial(int num) {
+        if (num <= 1) return 1;
+        return num * factorial(num - 1);
     }
 }
