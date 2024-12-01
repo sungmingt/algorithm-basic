394b8f983a9a4be02a4ef676546661eea18edc9e
sungmingt <qotjdals8313@naver.com>
2024-12-01 20:45:24 +0900

    boj_7569_BFS

--- src/main/java/basic/algorithm/baekjoon/bfs/gold/boj_7569_BFS.java
+++ src/main/java/basic/algorithm/baekjoon/bfs/gold/boj_7569_BFS.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class 토마토2_7569_BFS {
+public class boj_7569_BFS {
 
     static int[][][] arr;
     static Queue<Tomato> q;
@@ -16,9 +16,8 @@ public class 토마토2_7569_BFS {
     static int[] dH = {0, 0, 0, 0, -1, 1};
     static int[] dX = {-1, 1, 0, 0, 0, 0};
     static int[] dY = {0, 0, -1, 1, 0, 0};
-    static int N;
-    static int M;
-    static int H;
+    static int N, M, H;
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
         //위, 아래, 왼, 오, 앞, 뒤 6가지 방향으로 영향
@@ -28,7 +27,7 @@ public class 토마토2_7569_BFS {
         //1. 모두 익어있다 -> 0
         //2. 모두 익지 못한다 -> -1
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
         M = Integer.parseInt(st.nextToken()); //세로
         N = Integer.parseInt(st.nextToken()); //가로
@@ -42,18 +41,17 @@ public class 토마토2_7569_BFS {
         //배열 입력
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < N; j++) {
-                StringTokenizer st2 = new StringTokenizer(br.readLine());
+                st = new StringTokenizer(br.readLine());
 
                 for (int k = 0; k < M; k++) {
-                    Integer value = Integer.valueOf(st2.nextToken());
-                    arr[i][j][k] = value;
+                    int input = Integer.parseInt(st.nextToken());
+                    arr[i][j][k] = input;
 
                     //익은 토마토이면
-                    if (value == 1) {
+                    if (input == 1) {
                         q.offer(new Tomato(i, j, k));
-                        visited[i][j][k] = 1;
                         doneCount++;
-                    } else if (value == -1) {
+                    } else if (input == -1) {
                         doneCount++;
                     }
                 }
@@ -75,7 +73,7 @@ public class 토마토2_7569_BFS {
                 int newY = curY + dY[i];
 
                 //이동 가능한 경우
-                if (canMove(newH, newX, newY)) {
+                if (isMovable(newH, newX, newY)) {
                     //방문 안 한 경우
                     if (visited[newH][newX][newY] == 0) {
                         //안 익었을 경우
@@ -86,7 +84,7 @@ public class 토마토2_7569_BFS {
 
                             //모두 익으면
                             if (doneCount == N * M * H) {
-                                System.out.println(visited[newH][newX][newY] - 1);
+                                System.out.println(visited[newH][newX][newY]);
                                 return;
                             }
                         }
@@ -99,13 +97,14 @@ public class 토마토2_7569_BFS {
         System.out.println(-1);
     }
 
-    static boolean canMove(int newH, int newX, int newY) {
-        return 0 <= newH && newH < H && 0 <= newX && newX < N && 0 <= newY && newY < M;
+    static boolean isMovable(int h, int x, int y) {
+        return ((0 <= h && h < H) && (0 <= x && x < N) && (0 <= y && y < M));
     }
-    static class Tomato{
-        int h;
+
+    static class Tomato {
         int x;
         int y;
+        int h;
 
         Tomato(int h, int x, int y) {
             this.h = h;
