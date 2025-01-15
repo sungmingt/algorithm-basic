4990a629bccc183edc0fae33b0b3a61c284e0327
sungmingt <qotjdals8313@naver.com>
2025-01-15 13:08:08 +0900

    boj_2563_simulation

---
+++ src/main/java/basic/algorithm/baekjoon/bronze/boj_2563_simulation.java
@@ -0,0 +1,35 @@
+package basic.algorithm.baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_2563_simulation {
+
+    static boolean[][] sketch = new boolean[101][101];
+    static int total;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int k = x; k < x + 10; k++) {
+                for (int p = y; p < y + 10; p++) {
+
+                    if (!sketch[k][p]) {
+                        sketch[k][p] = true;
+                        total++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(total);
+    }
+}
