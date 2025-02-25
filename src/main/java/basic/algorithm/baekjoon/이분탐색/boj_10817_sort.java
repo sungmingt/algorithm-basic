4cf782f11c307b831fbca5008ad0e0a9e54c546f
sungmingt <qotjdals8313@naver.com>
2025-02-25 11:46:51 +0900

    boj_10817_sort

---
+++ src/main/java/basic/algorithm/baekjoon/이분탐색/boj_10817_sort.java
@@ -0,0 +1,24 @@
+package basic.algorithm.baekjoon.이분탐색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_10817_sort {
+
+    static int[] arr = new int[3];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
