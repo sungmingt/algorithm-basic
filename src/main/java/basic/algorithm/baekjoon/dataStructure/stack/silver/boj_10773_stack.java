61c270f01a07459924e4cd6db57ae24339cd54c0
sungmingt <qotjdals8313@naver.com>
2025-03-12 11:32:16 +0900

    boj_10773_stack

--- src/main/java/basic/algorithm/baekjoon/dataStructure/stack/silver/boj_10773_stack.java
+++ src/main/java/basic/algorithm/baekjoon/dataStructure/stack/silver/boj_10773_stack.java
@@ -13,7 +13,6 @@ public class boj_10773_stack {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         K = Integer.parseInt(br.readLine());
-
         stack = new Stack<>();
 
         for (int i = 0; i < K; i++) {
