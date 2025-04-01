6d519801c3ed70541d224888f52fe0a0b2f9a678
sungmingt <qotjdals8313@naver.com>
2025-04-01 15:41:16 +0900

    boj_1351

--- src/main/java/basic/algorithm/baekjoon/dataStructure/boj_1351.java
+++ src/main/java/basic/algorithm/baekjoon/dataStructure/boj_1351.java
@@ -1,4 +1,4 @@
-package basic.algorithm.baekjoon.dataStructure.silver;
+package basic.algorithm.baekjoon.dataStructure;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,26 +12,24 @@ public class boj_1351 {
     static long N, P, Q;
     static Map<Long, Long> map = new HashMap<>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Long.parseLong(st.nextToken());
         P = Long.parseLong(st.nextToken());
         Q = Long.parseLong(st.nextToken());
 
-        //A0 = 1
-        //Ai = A⌊i/P⌋ + A⌊i/Q⌋ (i ≥ 1)
-
-        map.put(0L, 1L);
-        find(N);
-        System.out.println(map.get(N));
+        System.out.println(solve(N));
     }
 
-    public static long find(long n) {
-        if (map.containsKey(n)) return map.get(n);
-        long first = find((long) Math.floor(n / P));
-        long second = find((long) Math.floor(n / P));
-        map.put(n, first + second);
-        return first + second;
+    static long solve(long num) {
+        if(num == 0) return 1;
+        if(map.containsKey(num)) return map.get(num);
+
+        long a = num / P;
+        long b = num / Q;
+
+        map.put(num, solve(a)+solve(b));
+        return map.get(num);
     }
 }
