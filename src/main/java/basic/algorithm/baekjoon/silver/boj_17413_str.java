a64c40c784aeb5cc8e53ab022a6fd772e7e0a736
sungmingt <qotjdals8313@naver.com>
2025-04-09 11:56:13 +0900

    boj_17413_str

---
+++ src/main/java/basic/algorithm/baekjoon/silver/boj_17413_str.java
@@ -0,0 +1,67 @@
+package basic.algorithm.baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class boj_17413_str {
+
+    static String str;
+    static Stack<Character> word;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        str = br.readLine();
+        word = new Stack<>();
+
+        //문자열의 처음부터 시작
+        //차례로 stack에 push
+
+        //공백 또는 태그가 나오면 -> stack의 값들을 차례로 꺼내서 정답에 이어붙인다.
+        //공백이 나온다 -> 공백을 그대로 이어붙인다.
+        //태그가 나온다 -> 태그가 끝날때까지 그대로 이어붙인다.
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            if (c == '<') {
+
+                while (!word.isEmpty()) {
+                    sb.append(word.pop());
+                }
+
+                sb.append(c);
+
+                while (true) {
+                    i++;
+
+                    char t = str.charAt(i);
+
+                    if (t == '>') {
+                        sb.append(t);
+                        break;
+                    } else {
+                        sb.append(t);
+                    }
+                }
+
+            } else if (c == ' ') {
+                while (!word.isEmpty()) {
+                    sb.append(word.pop());
+                }
+
+                sb.append(c);
+            } else {
+                word.push(c);
+            }
+        }
+
+        while (!word.isEmpty()) {
+            sb.append(word.pop());
+        }
+
+        System.out.println(sb);
+    }
+}
