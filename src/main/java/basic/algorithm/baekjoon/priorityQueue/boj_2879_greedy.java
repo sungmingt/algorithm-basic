2af965dd8abdb7208911fde9e6f41f4674145edd
sungmingt <qotjdals8313@naver.com>
2026-02-02 11:20:19 +0900

    boj_2879_greedy

--- src/main/java/basic/algorithm/baekjoon/priorityQueue/boj_2879_greedy.java
+++ src/main/java/basic/algorithm/baekjoon/priorityQueue/boj_2879_greedy.java
@@ -16,6 +16,7 @@ public class boj_2879_greedy {
         A = new int[N];
         B = new int[N];
 
+        //input
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
@@ -27,8 +28,21 @@ public class boj_2879_greedy {
         }
 
         int answer = 0;
-        int prev = 0; //diff[i-1]
+        int prev = 0;
 
+        //이전 작업과 현재 작업의 변화 방식에 따라 다르게 처리한다.
+        //1. 부호가 같을때
+        //  - 현재 작업량 <= 이전 작업량
+        //      - 이때는 이전 작업에서 현재 작업까지 모두 처리됐다고 보면 되기때문에, 별도의 작업을 하지 않는다.
+        //  - 현재 작업량 > 이전 작업량
+        //      - 이때는 이전 작업량에서 추가 작업이 생겼기 때문에, 추가 작업을 해준다.
+        //2. 부호가 다를때
+        //  - 현재 작업량만큼 추가 작업을 해준다.
+
+        //핵심: 처음에는 다른 부호가 나올때까지 탐색하고 최솟값을 기준으로 처리하려고 했지만,
+        // 여러 구간을 탐색할 필요없이 2개씩 비교해서 앞선 작업량이 더 많다면 현재 작업은 이미 포함되어 처리되었다고 생각하면 된다.
+
+        //greedy
         for (int i = 0; i < N; i++) {
             int cur = B[i] - A[i];
 
