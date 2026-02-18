45310b8948f5889bd7d75b8ece6920e1b8ed5d5e
sungmingt <qotjdals8313@naver.com>
2026-02-19 00:01:09 +0900

    leetCode_238_productOfArrayExceptSelf_prefixSum

---
+++ src/main/java/basic/algorithm/leetcode/leetCode_238_productOfArrayExceptSelf_prefixSum.java
@@ -0,0 +1,30 @@
+package basic.algorithm.leetcode;
+
+public class leetCode_238_productOfArrayExceptSelf_prefixSum {
+
+    //https://leetcode.com/problems/product-of-array-except-self/description/
+
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+
+        left[0] = 1;
+        for (int i = 1; i < len; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        right[len - 1] = 1;
+        for(int i = len - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        int[] answer = new int[len];
+
+        for(int i = 0; i < len; i++) {
+            answer[i] = left[i] * right[i];
+        }
+
+        return answer;
+    }
+}
